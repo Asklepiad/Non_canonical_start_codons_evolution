f1799bcf622b00777f4e06e4bdfee20b64f13f6e
Asklepiad <bogdan.sotnikov.1999@mail.ru>
2023-04-30 22:04:25 +0600

    Minor debugging

--- C_psittaci_folder/scripts/Organisms_parsing02.py
+++ C_psittaci_folder/scripts/Organisms_parsing02.py
@@ -7,6 +7,7 @@ import argparse
 import shutil
 import re
 import json
+from itertools import chain
 
 parser = argparse.ArgumentParser()
 parser.add_argument("json_path", type=str)
@@ -25,7 +26,7 @@ for bac, ids in organism_list.items():
         last_letter = 9
     name = f"{orglist[0][0]}_{orglist[-1][:last_letter]}"
     if not os.path.exists(f"../data/jsons/{name}.json"):
-        jdir = [bac, {bac: ids}]
+        jdir = [bac, {bac: list(chain.from_iterable(ids))}]
         new_file = json.dumps(jdir)
         with open(f"../data/jsons/{name}.json", "w") as jspc2:
             jspc2.write(new_file)
